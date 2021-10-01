9db5da6819cf85f066ee77c0848afed2ea4996ab
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-01 16:16:55 +0000

    Static_Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'home' => "static_pages#home"
+  get 'help' => "static_pages#help"
+  get 'about' => "static_pages#about"
+  get 'contact' => "static_pages#contact"
+  get 'hello-world' => "foo#hello"
   resources :tweets
   resources :users
-  get 'hello-world' => "foo#hello"
 end
