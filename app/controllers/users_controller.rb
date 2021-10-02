42fdc121f52971fdbf25a7bbb977bb5e9ad404a4
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-02 16:39:28 +0000

    validation and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @User.save
-      # 保存成功の場合ここで扱う。
+    if @user.save
+      redirect_to @user
     else
       render 'new'
     end
