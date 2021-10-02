b29befcbeea66880b740faf144d1c160a179d2c0
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-02 07:00:57 +0000

    user controllerand model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
-  get 'hello-world' => "foo#hello"
-  resources :tweets
-  resources :users
+  get 'signup' => "users#new"
 end
